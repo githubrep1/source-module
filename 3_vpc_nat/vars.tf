e6cbb0e3de7c0b779bf5f2d7cad801997446b17b
githubrep1 <93478438+githubrep1@users.noreply.github.com>
2022-07-01 07:20:38 +0530

    Update vars.tf

--- 3_vpc_nat/vars.tf
+++ 3_vpc_nat/vars.tf
@@ -13,3 +13,33 @@ variable "PATH_TO_PUBLIC_KEY" {
 variable "PATH_TO_PRIVATE_KEY" {
   default = "~/.ssh/devopsmonks.pem"
 }
+
+variable "vpc_cidr" {
+  type        = string
+  description = "The IP range to use for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_numbers" {
+  type = map(number)
+ 
+  description = "Map of AZ to a number that should be used for public subnets"
+ 
+  default = {
+    "eu-west-1a" = 1
+    "eu-west-1b" = 2
+    "eu-west-1c" = 3
+  }
+}
+ 
+variable "private_subnet_numbers" {
+  type = map(number)
+ 
+  description = "Map of AZ to a number that should be used for private subnets"
+ 
+  default = {
+    "eu-west-1a" = 4
+    "eu-west-1b" = 5
+    "eu-west-1c" = 6
+  }
+}
