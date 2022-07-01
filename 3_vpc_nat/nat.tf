2575cc76e869332e498349dafdb211180fa8f4f3
githubrep1 <93478438+githubrep1@users.noreply.github.com>
2022-07-01 16:40:32 +0530

    Update nat.tf

--- 3_vpc_nat/nat.tf
+++ 3_vpc_nat/nat.tf
@@ -4,38 +4,32 @@ resource "aws_eip" "nat" {
 }
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = "${aws_eip.nat.id}"
-  
-  subnet_id = aws_subnet.public[element(keys(aws_subnet.public), 0)].id
-
+  subnet_id = "${aws_subnet.main-public-1.id}"
   depends_on = [aws_internet_gateway.main-gw]
 }
 
 # VPC setup for NAT
-resource "aws_route_table" "private" {
+resource "aws_route_table" "main-private" {
     vpc_id = "${aws_vpc.main.id}"
-    
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = "${aws_nat_gateway.nat-gw.id}"
+    }
+
     tags = {
         Name = "main-private-1"
     }
 }
-
-# Private Route
-resource "aws_route" "private" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat-gw.id
-}
-
 # route associations private
-
-resource "aws_route_table_association" "private" {
-  for_each  = aws_subnet.private
-  subnet_id = aws_subnet.private[each.key].id
- 
-  route_table_id = aws_route_table.private.id
+resource "aws_route_table_association" "main-private-1-a" {
+    subnet_id = "${aws_subnet.main-private-1.id}"
+    route_table_id = "${aws_route_table.main-private.id}"
 }
-
-
-
-
-
+resource "aws_route_table_association" "main-private-2-a" {
+    subnet_id = "${aws_subnet.main-private-2.id}"
+    route_table_id = "${aws_route_table.main-private.id}"
+}
+resource "aws_route_table_association" "main-private-3-a" {
+    subnet_id = "${aws_subnet.main-private-3.id}"
+    route_table_id = "${aws_route_table.main-private.id}"
+}
