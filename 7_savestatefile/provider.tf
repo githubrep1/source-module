1ed08e3cf91be44d9b502d248d065ab89906ca08
githubrep1 <93478438+githubrep1@users.noreply.github.com>
2022-07-02 11:39:30 +0530

    Update provider.tf

--- 7_savestatefile/provider.tf
+++ 7_savestatefile/provider.tf
@@ -1,15 +1,5 @@
 terraform {
   
-  backend "s3" {
-    encrypt=true
-    bucket = "devopsmonkss-terraform-state-storage"
-    dynamodb_table = "devopsmonks-terraform-state-lock"
-    key    = "state-lock-storage.keypath"
-    region = "eu-west-1"
-    # access_key = "<aws_access_key>"
-    # secret_key = "<aws_secret_key>"
-  }
-
 
   required_providers {
     aws = {
