119d5f8cd33315f26035a67c7a10c1bf5c0de3f5
githubrep1 <93478438+githubrep1@users.noreply.github.com>
2022-07-03 13:54:48 +0530

    Update vars.tf

--- 10_iam_user_groups/vars.tf
+++ 10_iam_user_groups/vars.tf
@@ -5,15 +5,27 @@ variable "AWS_REGION" {
 variable "AWS_PROFILE" {
   default = "terraform"
 }
-variable "aws_iam_group" {}
-    
-variable "aws_iam_policy_attachment" {}
+variable "aws_iam_group" {
+    default = "administrators"
+}    
 
-variable "aws_iam_user_1" {}
+variable "aws_iam_policy_attachment" {
+      default = "administrators-attach"
+}
 
-variable "aws_iam_user_2 {}
+variable "aws_iam_user_1" {
+default = "admin1"
+}
 
-variable "aws_iam_group_membership" {}
+variable "aws_iam_user_2 {
+default = "admin2"
+}
+
+variable "aws_iam_group_membership" {
+    default = "administrators-users"
+
+
+}
 
 
     
