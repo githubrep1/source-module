59292133aadffa9078e31621b3019cbfeaf173a1
githubrep1 <93478438+githubrep1@users.noreply.github.com>
2022-07-03 10:10:30 +0530

    Update vars.tf

--- 5_ebs_volumes/vars.tf
+++ 5_ebs_volumes/vars.tf
@@ -1,21 +1,15 @@
-variable "AWS_REGION" {
-  default = "eu-west-1"
-}
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "~/.ssh/devopsmonks.pub"
-}
+variable "AWS_REGION" {}
+variable "PATH_TO_PUBLIC_KEY" {}
 
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "~/.ssh/devopsmonks.pem"
-}
+variable "PATH_TO_PRIVATE_KEY" {}
 
-variable "instance_type" {
-  default = "t2.micro"
-}
+variable "instance_type" {}
 
-variable "AWS_PROFILE" {
-  default = "terraform"
-}
+variable "AWS_PROFILE" {}
+
+variable "availability_zone" {}
+variable "instance_type" {}
+
 variable "AMIS" {
   type = map(string)
   default = {
