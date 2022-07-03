5faa778dc36d953d5cd23da5e55a162bd5060462
githubrep1 <93478438+githubrep1@users.noreply.github.com>
2022-07-03 12:46:33 +0530

    Update vars.tf

--- 8_route53/vars.tf
+++ 8_route53/vars.tf
@@ -3,4 +3,24 @@ variable "AWS_REGION" {}
 
 variable "AWS_PROFILE" {}
 
+variable "route53_name" {
+  default = "devopsmonks.com"
+}
+
+variable "route53_server_record" {
+  
+default = "server1.devopsmonks"
+  
+}
+
+variable "route53_reconrd_name"{
+
+default = "www.devopsmonks"
+
+variable "route53_mail_record_name" {
+
+  default = "devopsmonks"
+}
+
+
 
