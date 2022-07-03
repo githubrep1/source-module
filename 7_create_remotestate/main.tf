8b77eb41120cad813bfd3dd5b365933a39bf81c2
githubrep1 <93478438+githubrep1@users.noreply.github.com>
2022-07-03 11:47:22 +0530

    Update main.tf

--- 7_create_remotestate/main.tf
+++ 7_create_remotestate/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tf-state-storage" {
-    bucket = "devopsmonkssv-terraform-state-storage"
+    bucket = var.bucket
     acl    = "private"
     versioning {
       enabled = true
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "tf-state-storage" {
 }
 # create a dynamodb table for locking the state file
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "devopsmonks-terraform-state-lock"
+  name = var.name
   hash_key = "LockID"
   read_capacity = 20
   write_capacity = 20
