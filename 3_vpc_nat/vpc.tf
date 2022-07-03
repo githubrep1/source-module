235097727832c1decb4216a61b04a826ea12bf20
githubrep1 <93478438+githubrep1@users.noreply.github.com>
2022-07-03 08:37:45 +0530

    Update vpc.tf

--- 3_vpc_nat/vpc.tf
+++ 3_vpc_nat/vpc.tf
@@ -1,6 +1,6 @@
 # Internet VPC
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.cidr_block
     instance_tenancy = "default"
     enable_dns_support = "true"
     enable_dns_hostnames = "true"
